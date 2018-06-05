d42d0b9764e0448e518591fa0ad1dbeb08be0721
dgzornoza <dgzornoza@dgzornoza.com>
2018-06-05 21:50:37 +0200

    -- añadida toolkit b2jjson

---
+++ Asteroids/Asteroids/Game/GameConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asteroids.Game
+{
+    public class GameConfig
+    {
+        public const string mainCameraNodeName = "MainCamera";
+    }
+}
--- Asteroids/Asteroids/Game/UrhoApp.cs
+++ Asteroids/Asteroids/Game/UrhoApp.cs
@@ -11,14 +11,19 @@ namespace Asteroids.Game
 {
     public class UrhoApp : Application
     {
+        private MonoDebugHud _monoDebugHud;
 
         private Scene _scene;
         private Camera _camera;
 
+        private bool _drawDebug = true;
+
 
 
         [Preserve]
-        public UrhoApp(ApplicationOptions options = null) : base(options) { }
+        public UrhoApp(ApplicationOptions options = null) : base(options)
+        {
+        }
 
         static UrhoApp()
         {
@@ -29,44 +34,55 @@ namespace Asteroids.Game
             };
         }
 
+
+
+
         protected override void Start()
         {
+            Log.LogMessage += e => Debug.WriteLine($"[{e.Level}] {e.Message}");
             base.Start();
-            _createScene();
-            _setupViewport();
+
+#if DEBUG
+            _monoDebugHud = new MonoDebugHud(this);
+            _monoDebugHud.Show();
+#endif
+
+            this._createScene();
+            this._createCamera();
+            this._setupViewport();
+
+
+            this._subscribeToEvents();
+
+
+            Rube rube = new Rube();
+            rube.LoadWorld(this._scene);
         }
 
 
+
+        protected override void OnUpdate(float timeStep)
+        {
+        }
 
 
 
         private void _createScene()
         {
-            // https://discourse.urho3d.io/t/draw-a-line-with-custom-geometry-in-2d/3192/2
-
             // create scene
             this._scene = new Scene();
             this._scene.CreateComponent<Octree>();
-
-            this._createCamera();
-
-            // this._createBackground();
-
-            
-            Node ShipNode = this._scene.CreateChild("Ship1");
-            ShipNode.CreateComponent<ShipComponent>();
-            ShipNode.Position = new Vector3(0.0f, 0.0f, 0.0f);
+            this._scene.CreateComponent<DebugRenderer>();
         }
 
 
         private void _createCamera()
         {
             // Create camera
-            Node CameraNode = _scene.CreateChild("MainCamera");
-            CameraNode.Position = (new Vector3(0.0f, 0.0f, -10.0f));
+            Node CameraNode = _scene.CreateChild(GameConfig.mainCameraNodeName);
+            CameraNode.Position = (new Vector3(0.0f, 0.0f, -0.10f));
             this._camera = CameraNode.CreateComponent<Camera>();
             this._camera.Orthographic = true;
-
 
             var graphics = Graphics;
             // x = Screen Width (px)
@@ -76,37 +92,43 @@ namespace Asteroids.Game
             // this._camera.OrthoSize = graphics.Width / (((graphics.Width / graphics.Height) * 2) * 32);
 
             this._camera.OrthoSize = (float)graphics.Height * PixelSize;
-            // establecer el zoom segun la resolucion de diseño para asegurar visibilidad completa (zomm (1.0) para completa visibildiad en una resolucion 3:2 1080X720)
+            // set zoom with design resolution for view all sample (zomm (1.0) for view in 3:2 1080X720)
             this._camera.Zoom = 1.0f * Math.Min((float)graphics.Width / 1080.0f, (float)graphics.Height / 720.0f);
         }
 
 
-        private void _createBackground()
-        {
-            var cache = ResourceCache;
-            Sprite2D backgroundSprite = cache.GetSprite2D("Textures/stars.png");
 
-            if (backgroundSprite is null)
-            {
-                return;
-            }
-
-            Node backgroundNode = this._scene.CreateChild("StaticSprite2D");
-            backgroundNode.Position = new Vector3(0.0f, 0.0f, 0.0f);
-
-            StaticSprite2D backgroundNodeStaticSprite = backgroundNode.CreateComponent<StaticSprite2D>();
-            // blend mode
-            backgroundNodeStaticSprite.BlendMode = BlendMode.Alpha;
-            // backgroundNodeStaticSprite.Alpha = .5f;
-            // sprite
-            backgroundNodeStaticSprite.Sprite = backgroundSprite;
-        }
 
         private void _setupViewport()
         {
             Viewport viewport = new Viewport(Context, this._scene, this._camera, null);
             var renderer = Renderer;
+
             renderer.SetViewport(0, viewport);
+
+            // Set background color for the scene
+            // Zone zone = renderer.DefaultZone;
+            // zone.FogColor = (new Color(1f, 0.1f, 0.1f));
         }
+
+
+
+
+        private void _subscribeToEvents()
+        {
+            Engine.PostRenderUpdate += (PostRenderUpdateEventArgs obj) =>
+            {
+                // If draw debug mode is enabled, draw viewport debug geometry, which will show eg. drawable bounding boxes and skeleton
+                // bones. Note that debug geometry has to be separately requested each frame. Disable depth test so that we can see the
+                // bones properly
+                if (_drawDebug)
+                {
+                    this._scene.GetComponent<PhysicsWorld2D>().DrawDebugGeometry();
+                    Renderer.DrawDebugGeometry(false);
+                }
+            };
+        }
+
+
     }
 }
