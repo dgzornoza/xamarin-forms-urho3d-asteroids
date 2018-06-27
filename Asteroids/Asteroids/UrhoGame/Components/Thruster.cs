3d071e6210ef41a29bf439813b7c4bfb60f1fd0c
dgzornoza <dgzornoza@dgzornoza.com>
2018-06-27 22:51:25 +0200

    no message

--- Asteroids/Asteroids/UrhoGame/Components/Thruster.cs
+++ Asteroids/Asteroids/UrhoGame/Components/Thruster.cs
@@ -33,24 +33,16 @@ namespace Asteroids.UrhoGame.Components
 
         public void SetParametersFromBody(RigidBody2D body)
         {
-            // this.Node.Rotation2D = body.Node.Rotation2D -90;
-            //this._particleEmitter.Effect.Gravity = MathHelpers.DegreeToVector2(-this.Node.Rotation2D) * 3000;
+            // this.Node.Rotation2D = body.Node.Rotation2D - 90;
+            // Vector2 rotation2D = MathExtensions.DegreeToVector2(this.Node.Rotation2D) * 5;
+            // this._particleEmitter.Effect.ConstantForce = new Vector3(rotation2D);
 
-            this.Node.Rotation2D = body.Node.Rotation2D - 90;
-            Vector2 rotation2D = MathExtensions.DegreeToVector2(this.Node.Rotation2D) * 5;
-            this._particleEmitter.Effect.ConstantForce = new Vector3(rotation2D);
             this._particleEmitter.Effect.SizeAdd = (body.LinearVelocity.LengthFast * 15) / 15;
         }
 
         private void _initialize()
         {
             var cache = this.Application.ResourceCache;
-            //ParticleEffect2D particleEffect = cache.GetParticleEffect2D("Urho2D/Particles/thruster.pex");
-            //if (particleEffect == null) return;
-
-            //this._particleEmitter = this.Node.CreateComponent<ParticleEmitter2D>();
-            //this._particleEmitter.Effect = particleEffect; 
-
             ParticleEffect particleEffect = cache.GetParticleEffect("Particles/thruster.xml");
             if (particleEffect == null) return;
 
